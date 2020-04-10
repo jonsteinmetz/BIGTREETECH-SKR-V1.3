d453df406dd061c3e866607d6251f54d77b94c7d
Jon Steinmetz <jon@pixelresearchlabs.com>
2020-04-09 19:41:11 -0500

    Enable linear advance.

--- BTT SKR V1.3/firmware/Marlin-bugfix-2.0.x/Marlin/Configuration.h
+++ BTT SKR V1.3/firmware/Marlin-bugfix-2.0.x/Marlin/Configuration.h
@@ -780,7 +780,7 @@
   #endif
 #endif
 
-#define DEFAULT_EJERK    5.0  // May be used by Linear Advance
+#define DEFAULT_EJERK    0.6  // May be used by Linear Advance
 
 /**
  * Junction Deviation Factor
